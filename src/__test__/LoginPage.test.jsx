bc467bd32f613a63f14e665eb52fa94a82602be0
Jutipong Puntuleng <p.jutipong13@gmail.com>
2025-06-20 11:02:03 +0700

    Test: Fix file path

--- src/__test__/LoginPage.test.jsx
+++ src/__test__/LoginPage.test.jsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import LoginPage from '../LoginPage'
+import LoginPage from '../pages/LoginPage'
 
 describe('LoginPage', () => {
   it('renders without crashing', () => {
